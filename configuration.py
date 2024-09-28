b437dbed73fcdd32214dc49a470812bc41a7dd1a
akvdo <akvdo@yandex.ru>
2024-09-28 21:22:47 +0300

    Подготовлен README.md

--- configuration.py
+++
@@ -1,8 +0,0 @@
-# URL_SERVICE хранит базовый URL тестового стенда, который используется для доступа к API Яндекс.Прилавка
-URL_SERVICE = "https://f916b4ea-21c8-4bed-9d09-e9d901201a6c.serverhub.praktikum-services.ru"
-
-# CREATE_USER_PATH хранит путь к API-методу для создания нового пользователя.
-CREATE_USER_PATH = "/api/v1/users/"
-
-# MAIN_KITS_PATH хранит путь к API-методу  для создания набора внутри конкретного пользователя.
-MAIN_KITS_PATH = "/api/v1/kits"
